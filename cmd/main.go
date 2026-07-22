9ec4b8eed301f448edfe6333c0d89830f10c83a9
I-Maged <magedhanafy@gmail.com>
2026-07-22 23:43:09 +0300

    db connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
 	"github.com/I-Maged/ecom/cmd/api"
+	"github.com/I-Maged/ecom/config"
+	"github.com/I-Maged/ecom/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.ENVS.DBUser,
+		Passwd:               config.ENVS.DBPassword,
+		Addr:                 config.ENVS.DBAddress,
+		DBName:               config.ENVS.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
