c96476964a430c17eab5fd8e0e46f78330a75f6d
I-Maged <magedhanafy@gmail.com>
2026-07-23 05:55:59 +0300

    register user

--- service/user/routes.go
+++ service/user/routes.go
@@ -1,16 +1,21 @@
 package user
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/I-Maged/ecom/service/auth"
+	"github.com/I-Maged/ecom/types"
+	"github.com/I-Maged/ecom/utils"
 	"github.com/gorilla/mux"
 )
 
 type Handler struct {
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
@@ -22,4 +27,38 @@ func (h *Handler) handleLogin(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {}
+func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
+	// get JSON payload
+	var payload types.RegisterUserPayload
+	err := utils.ParseJSON(r, payload)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+
+	// Check if user exists
+	_, err = h.store.GetUserByEmail(payload.Email)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("User with email %s already exist", payload.Email))
+		return
+	}
+
+	// Hash password
+	hashedPassword, err := auth.HashPassword(payload.Password)
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+	}
+
+	// Create new user
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		Password:  hashedPassword,
+	})
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
+}
